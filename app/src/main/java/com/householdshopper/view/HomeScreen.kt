0c2e0bf31db4d6809e9b4ceea8549c761a19a9e9
Nikola Muženjak <nmuzenjak@etfos.hr>
2024-06-27 11:59:08 +0200

    Added new icons for NavigationBar

--- app/src/main/java/com/householdshopper/view/HomeScreen.kt
+++ app/src/main/java/com/householdshopper/view/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.householdshopper.view
 
-import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,19 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -31,21 +22,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.householdshopper.view.recycleView.ShoppingListItem
+import com.householdshopper.view.items.ShoppingListItem
 import com.householdshopper.viewmodel.HomeViewModel
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.platform.LocalContext
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.coroutineScope
+import androidx.compose.ui.res.painterResource
+import com.householdshopper.R
+import com.householdshopper.view.components.TopBar
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,12 +48,8 @@ fun HomeScreen(
 
     val context = LocalContext.current
 
-    var isExpanded by remember {
-        mutableStateOf(false)
-    }
-
     LaunchedEffect(Unit) {
-        viewModel.sendMessage(context)
+        viewModel.sendMessage(context, title = "Test title", body = "Test body")
     }
 
     Column(
@@ -74,67 +57,7 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CenterAlignedTopAppBar(
-            title = {
-                Text(
-                text = household.name,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
-            )},
-            navigationIcon = {
-                IconButton(onClick = { navController.popBackStack() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Go back"
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = { isExpanded = !isExpanded }) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = "Menu"
-                    )
-                }
-
-                DropdownMenu(
-                    expanded = isExpanded,
-                    onDismissRequest = { isExpanded = false }
-                ) {
-                    DropdownMenuItem(
-                        text = {
-                            Row {
-                                Icon(
-                                    imageVector = Icons.Default.AccountBox,
-                                    contentDescription = "Household",
-                                    modifier = Modifier.padding(end = 8.dp)
-                                )
-                                Text("Household")
-                            }},
-                        onClick = {
-                            isExpanded = false
-                            navController.navigate("household")
-                        })
-                    DropdownMenuItem(
-                        text = {
-                            Row {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Filled.ExitToApp,
-                                    contentDescription = "Logout",
-                                    modifier = Modifier.padding(end = 8.dp)
-                                )
-                                Text("Logout")
-                            }
-                        },
-                        onClick = {
-                            FirebaseAuth.getInstance().signOut()
-                            isExpanded = false
-                            navController.navigate("login")
-                        }
-                    )
-                }
-            }
-        )
+        TopBar(title = household.name, navController = navController)
         Box(modifier = Modifier
             .fillMaxSize()
             .weight(1f)
@@ -173,13 +96,13 @@ fun HomeScreen(
         }
         NavigationBar {
             NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = "Active lists") },
+                icon = { Icon(painter = painterResource(id = R.drawable.outline_shopping_bag_24), contentDescription = "Active lists") },
                 label = { Text("Active lists") },
                 selected = selectedItem == 0,
                 onClick = { viewModel.updateLists(0) }
             )
             NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = "All lists") },
+                icon = { Icon(painter = painterResource(id = R.drawable.outline_shopping_cart_24), contentDescription = "All lists") },
                 label = { Text("All lists") },
                 selected = selectedItem == 1,
                 onClick = { viewModel.updateLists(1) }
